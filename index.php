2d1f8fc415f5742ffb419fc5c2d47937c0abaf09
carlio42 <sb1510@outlook.com>
2018-07-20 15:13:37 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
 		<a href='ftpdownload.php'>FTP Download</a>
 		<br><br>
 		<a href='ftpupload.php'>FTP Upload</a>
+		<form action='ftpdownload.php' method='POST' enctype='multipart/form-data'>
+			<input type='file' name='file'>
+			<button type='submit' name='submit'>Upload</button>
+		</form>
 	</body>
 </html>
